6f39065138d0fe8b4c8e9841edf9183b2c276fb3
Gligor <pecevgligor@gmail.com>
2020-10-29 14:27:44 +0800

    apply css to elements

--- src/App.svelte
+++ src/App.svelte
@@ -268,129 +268,149 @@
     </script>
 
     <main>
-      <div id='whole-page'>
+
+      <div class='content-page'>
+        <div class='content'>
         <div id='head-section'>
-          <div id='flex-row'>
-            <h3>PFR REPORTS</h3>
-            <div id='head-sections-buttons'>
-              <select on:click={()=>sentEvents('SELECT_REPORT_EVENT')}>
-                <option value='undefined'>Select Reports</option>
-              </select>
-              <button class='btn' on:click={()=>sentEvents('NEW_REPORT_EVENT')}>New Report</button>
+            <div id='flex-row'>
+              <h3>PFR REPORTS</h3>
+              <div id='head-sections-buttons'>
+                <select on:click={()=>sentEvents('SELECT_REPORT_EVENT')}>
+                  <option value='undefined'>Select Reports</option>
+                </select>
+                <button class='btn' on:click={()=>sentEvents('NEW_REPORT_EVENT')}>New Report</button>
+              </div>
             </div>
           </div>
-        </div>
 
-        {#if makeMachine.context.showAbsoluteMenu}
-        <div class='modal-wrap'>
-          <div class='modal-bg'>
-            <div class='modal-content' style='position:absolute; left:50%; top:50%; transform: translate(-50%, -50%)'>
-              <div class='modal-header'>
-                <h2 id='center-div-main-head-h'>New PFR</h2>
-              </div>
-                <div class='modal-body'>
-                  <p>Please name your new PRF report</p>
-                  <input bind:value={inputName} />
-                  <label>
-                    <input type='checkbox' bind:checked={makeMachine.context.checkBoxConfirm} />
-                    By checking this box, you agree to creating a new PFR report for 1
-                    Credit
-                  </label>
-                  {#if makeMachine.context.checkBoxConfirm}
-                  <button class='btn' id='btn-1' on:click={() => sentEvents('SHOWREPORTPAGE')}>Create new PFR report</button>
-                  {/if}
-                </div>
-                </div>
+          {#if makeMachine.context.showAbsoluteMenu}
+            <div class='rw'>
+              <div class='cl-50 selected'>
+                <div class='card'>
+                  <div class='card-header'></div>
+                  <div class='card-body'>
+                    <h3 class='card-title'>Selected Items</h3>
+                    <div class='card-options'>
+                      <form>
+                        <input type='text' bind:value={searchKW} />
+                        <button class='btn'>Search</button>
+                      </form>
+                      <button class='btn'>All</button>
+                    </div>
+                  </div>
                 </div>
               </div>
-            {/if}
+
+              <div class='cl-50 unselected'>
+                <div class='card'>
+                  <div class='card-header'></div>
+                  <div class='card-body'>
+                    <h3 class='card-title'>Unselected Items</h3>
+                    <div class='card-options'>
+                      <button class='btn'>All</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          </main>
+          {/if}
+        </div>
+      </div>
+
+
+    </main>
 
     <style>
-      #whole-page{
-        width: 100vw;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-      #head-section{
-        width: 70%;
-        height: 40px;
-      }
-      #flex-row {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-      }
-      #head-sections-buttons {
-        margin-right: 40px;
-      }
-      h3 {
-        margin-left: 50px;
-      }
-      #absolute-center-div {
-        border: 1px solid black;
-      }
-    .btn {
-      border-radius: 3px;
-      background-color: #ff2c54;
-      border: 0;
-      color: white;
-      font-size: 0.8rem;
-      cursor: pointer;
-      outline: none;
-      padding: 8px;
-      min-width: 80px;
-      box-shadow: 0 0;
-      margin-right: 5px;
-      box-shadow: 0 0.125rem 0.25rem 0 rgba(58, 59, 69, 0.2) !important;
-      transition: all 0.3s ease;
+  .content-page {
+    min-height: 100vh;
+    z-index: 0;
+    overflow: hidden;
+    padding: 100px 30px 30px;
+    background: rgb(249, 250, 253);
   }
-  select {
-    -webkit-writing-mode: horizontal-tb !important;
-    text-rendering: auto;
-    color: -internal-light-dark(black, white);
-    letter-spacing: normal;
-    word-spacing: normal;
-    text-transform: none;
-    text-indent: 0px;
-    text-shadow: none;
-    display: inline-block;
-    text-align: start;
-    appearance: menulist;
-    box-sizing: border-box;
-    align-items: center;
-    white-space: pre;
-    -webkit-rtl-ordering: logical;
-    background-color: -internal-light-dark(rgb(255, 255, 255), rgb(59, 59, 59));
-    cursor: default;
-    margin: 0em;
-    font: 400 13.3333px Arial;
-    border-width: 1px;
-    border-style: solid;
-    border-color: -internal-light-dark(rgb(118, 118, 118), rgb(133, 133, 133));
-    border-image: initial;
-    border-radius: 0.25rem;
-    box-sizing: border-box;
-    padding: 7px 5px;
-    width: 150px;
+    #whole-page{
+      width: 100vw;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    #head-section{
+      width: 70%;
+      height: 40px;
+    }
+    #flex-row {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+    }
+    #head-sections-buttons {
+      margin-right: 40px;
+    }
+    h3 {
+      margin-left: 50px;
+    }
+    #absolute-center-div {
+      border: 1px solid black;
+    }
+.btn {
+    border-radius: 3px;
+    background-color: #ff2c54;
+    border: 0;
+    color: white;
+    font-size: 0.8rem;
+    cursor: pointer;
+    outline: none;
+    padding: 8px;
+    min-width: 80px;
+    box-shadow: 0 0;
     margin-right: 5px;
-    box-shadow: rgba(58, 59, 69, 0.2) 0px 0.125rem 0.25rem 0px !important;
-  }
-  .modal-wrap {
-    position: fixed;
-    width: 100%;
-    top: 0;
-    left: 0;
-    z-index: 999;
-    bottom: 0;
-    right: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    box-shadow: 0 0.125rem 0.25rem 0 rgba(58, 59, 69, 0.2) !important;
+    transition: all 0.3s ease;
+}
+select{
+  -webkit-writing-mode: horizontal-tb !important;
+  text-rendering: auto;
+  color: -internal-light-dark(black, white);
+  letter-spacing: normal;
+  word-spacing: normal;
+  text-transform: none;
+  text-indent: 0px;
+  text-shadow: none;
+  display: inline-block;
+  text-align: start;
+  appearance: menulist;
+  box-sizing: border-box;
+  align-items: center;
+  white-space: pre;
+  -webkit-rtl-ordering: logical;
+  background-color: -internal-light-dark(rgb(255, 255, 255), rgb(59, 59, 59));
+  cursor: default;
+  margin: 0em;
+  font: 400 13.3333px Arial;
+  border-width: 1px;
+  border-style: solid;
+  border-color: -internal-light-dark(rgb(118, 118, 118), rgb(133, 133, 133));
+  border-image: initial;
+  border-radius: 0.25rem;
+  box-sizing: border-box;
+  padding: 7px 5px;
+  width: 150px;
+  margin-right: 5px;
+  box-shadow: rgba(58, 59, 69, 0.2) 0px 0.125rem 0.25rem 0px !important;
+}
+.modal-wrap {
+  position: fixed;
+  width: 100%;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .modal-bg {
@@ -431,5 +451,85 @@ h2 {
 }
 #btn-1 {
   margin-top: 11px;
+}
+.rw{
+  display: flex;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  width: 100%;
+}
+.cl-50 {
+  width: 50%;
+  padding: 10px;
+  box-sizing: border-box;
+}
+/* .selected .card-body,
+.unselected .card-body {
+    min-height: 100px;
+    max-height: 600px;
+    overflow-y: auto;
+  } */
+  .card {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    min-width: 0;
+    word-wrap: break-word;
+    background-color: #fff;
+    background-clip: border-box;
+    border: 1px solid #e3e6f0;
+    border-radius: 0.35rem;
+    box-shadow: 0 0.15rem 1.75rem 0 rgba(58, 59, 69, 0.15) !important;
+}
+.card-header {
+    padding: 16px 20px;
+    margin-bottom: 0;
+    background-color: #f8f9fc;
+    border-bottom: 1px solid #e3e6f0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.card-body {
+    flex: 1 1 auto;
+    min-height: 1px;
+    padding: 20px;
+    display: flex;
+    flex-direction: row;
+}
+h3.card-title {
+    font-size: 16px;
+    font-weight: 500;
+    margin: 0;
+    color: #f92c51;
+    line-height: 1em;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.card-options {
+    display: flex;
+    align-items: center;
+}
+.icnbtn {
+    padding: 5.5px !important;
+    min-width: auto !important;
+}
+.search-btn {
+    border-radius: 3px;
+    background-color: #ff2c54;
+    border: 1px solid;
+    color: white;
+    font-size: 0.8rem;
+    cursor: pointer;
+    outline: none;
+    padding: 5.5px;
+    box-shadow: 0 0;
+    margin-left: -2px;
+}
+.icon {
+    height: 21px;
+    width: 21px;
 }
     </style>
